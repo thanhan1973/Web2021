61f63722ac1c4a2feed5af5d5371c7171fdd3c87
thanhan1973 <77916036+thanhan1973@users.noreply.github.com>
2021-01-24 16:03:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Music Online | Phan Thành An</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/menubar.css">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/trangchu.css">
+    <script src="./js/fontawesome.js"></script>
+</head>
+<body>
+<section class="nav-view">
+    <div class="nav-logo">
+        <img src="./img/logo.png" alt="">
+    </div>
+    <div class="nav-search">
+        <div>
+            <form action="">
+                <input type="text" placeholder="Tìm kiếm bài hát">
+                <button>
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+        </div>
+    </div>
+</section>
+<section class="welcome-banner">
+    <div class="canvas">
+        <div class="banner-play">
+            <div class="img-view">
+                <img src="./img/25.jpg" alt="">
+            </div>
+            <div class="banner-play-info">
+                <span class="time-title">7 THÁNG 12, 2019</span>
+                <H2 class="song-name">Yêu được không - Đức Phúc</H2>
+                <p class="info">Đức Phúc | 00:05:31</p>
+                <div class="player-bar">
+                    <i class="fas fa-play-circle btn-play"></i>
+                    <span class="time-start">00:00</span>
+                    <div class="time-bar"></div>
+                    <span class="time-end">05:31</span>
+                    <i class="fas fa-volume-down btn-volume"></i>
+                    <div class="volume-bar"></div>
+                </div>
+                <div class="dash"></div>
+                <ul class="menu-social">
+                    <li>
+                        <a href="">
+                            <i class="fas fa-heart"></i>
+                            <span>Like</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-share-alt"></i>
+                            <span>Share</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-download"></i>
+                            <span>Download</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="canvas last-song-section">
+    <h2 class="section-title">Bài Hát Mới</h2>
+    <div class="underline"></div>
+    <div class="last-song-body-row">
+        <div class="last-song-box">
+            <div class="music-thumb">
+                <img src="./img/5.jpg" alt="">
+            </div>
+            <div class="banner-play-info center">
+                <span class="time-title">7 THÁNG 12, 2019</span>
+                <H2 class="song-name">Yêu được không - Đức Phúc</H2>
+                <p class="info">Đức Phúc | 00:05:31</p>
+                <div class="player-bar">
+                    <i class="fas fa-play-circle btn-play"></i>
+                    <span class="time-start">00:00</span>
+                    <div class="time-bar"></div>
+                    <span class="time-end">05:31</span>
+                    <i class="fas fa-volume-down btn-volume"></i>
+                    <div class="volume-bar"></div>
+                </div>
+                <div class="dash"></div>
+                <ul class="menu-social">
+                    <li>
+                        <a href="">
+                            <i class="fas fa-heart"></i>
+                            <span>Like</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-share-alt"></i>
+                            <span>Share</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-download"></i>
+                            <span>Download</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="last-song-box">
+            <div class="music-thumb">
+                <img src="./img/6.jpg" alt="">
+            </div>
+            <div class="banner-play-info center">
+                <span class="time-title">7 THÁNG 12, 2019</span>
+                <H2 class="song-name">Yêu được không - Đức Phúc</H2>
+                <p class="info">Đức Phúc | 00:05:31</p>
+                <div class="player-bar">
+                    <i class="fas fa-play-circle btn-play"></i>
+                    <span class="time-start">00:00</span>
+                    <div class="time-bar"></div>
+                    <span class="time-end">05:31</span>
+                    <i class="fas fa-volume-down btn-volume"></i>
+                    <div class="volume-bar"></div>
+                </div>
+                <div class="dash"></div>
+                <ul class="menu-social">
+                    <li>
+                        <a href="">
+                            <i class="fas fa-heart"></i>
+                            <span>Like</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-share-alt"></i>
+                            <span>Share</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-download"></i>
+                            <span>Download</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    <div class="last-song-body-row">
+        <div class="last-song-box">
+            <div class="music-thumb">
+                <img src="./img/7.jpg" alt="">
+            </div>
+            <div class="banner-play-info center">
+                <span class="time-title">7 THÁNG 12, 2019</span>
+                <H2 class="song-name">Yêu được không - Đức Phúc</H2>
+                <p class="info">Đức Phúc | 00:05:31</p>
+                <div class="player-bar">
+                    <i class="fas fa-play-circle btn-play"></i>
+                    <span class="time-start">00:00</span>
+                    <div class="time-bar"></div>
+                    <span class="time-end">05:31</span>
+                    <i class="fas fa-volume-down btn-volume"></i>
+                    <div class="volume-bar"></div>
+                </div>
+                <div class="dash"></div>
+                <ul class="menu-social">
+                    <li>
+                        <a href="">
+                            <i class="fas fa-heart"></i>
+                            <span>Like</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-share-alt"></i>
+                            <span>Share</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-download"></i>
+                            <span>Download</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="last-song-box">
+            <div class="music-thumb">
+                <img src="./img/8.jpg" alt="">
+            </div>
+            <div class="banner-play-info center">
+                <span class="time-title">7 THÁNG 12, 2019</span>
+                <H2 class="song-name">Yêu được không - Đức Phúc</H2>
+                <p class="info">Đức Phúc | 00:05:31</p>
+                <div class="player-bar">
+                    <i class="fas fa-play-circle btn-play"></i>
+                    <span class="time-start">00:00</span>
+                    <div class="time-bar"></div>
+                    <span class="time-end">05:31</span>
+                    <i class="fas fa-volume-down btn-volume"></i>
+                    <div class="volume-bar"></div>
+                </div>
+                <div class="dash"></div>
+                <ul class="menu-social">
+                    <li>
+                        <a href="">
+                            <i class="fas fa-heart"></i>
+                            <span>Like</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-share-alt"></i>
+                            <span>Share</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-download"></i>
+                            <span>Download</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="last-song-body-row">
+        <div class="last-song-box">
+            <div class="music-thumb">
+                <img src="./img/9.jpg" alt="">
+            </div>
+            <div class="banner-play-info center">
+                <span class="time-title">7 THÁNG 12, 2019</span>
+                <H2 class="song-name">Yêu được không - Đức Phúc</H2>
+                <p class="info">Đức Phúc | 00:05:31</p>
+                <div class="player-bar">
+                    <i class="fas fa-play-circle btn-play"></i>
+                    <span class="time-start">00:00</span>
+                    <div class="time-bar"></div>
+                    <span class="time-end">05:31</span>
+                    <i class="fas fa-volume-down btn-volume"></i>
+                    <div class="volume-bar"></div>
+                </div>
+                <div class="dash"></div>
+                <ul class="menu-social">
+                    <li>
+                        <a href="">
+                            <i class="fas fa-heart"></i>
+                            <span>Like</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-share-alt"></i>
+                            <span>Share</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-download"></i>
+                            <span>Download</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="last-song-box">
+            <div class="music-thumb">
+                <img src="./img/10.jpg" alt="">
+            </div>
+            <div class="banner-play-info center">
+                <span class="time-title">7 THÁNG 12, 2019</span>
+                <H2 class="song-name">Yêu được không - Đức Phúc</H2>
+                <p class="info">Đức Phúc | 00:05:31</p>
+                <div class="player-bar">
+                    <i class="fas fa-play-circle btn-play"></i>
+                    <span class="time-start">00:00</span>
+                    <div class="time-bar"></div>
+                    <span class="time-end">05:31</span>
+                    <i class="fas fa-volume-down btn-volume"></i>
+                    <div class="volume-bar"></div>
+                </div>
+                <div class="dash"></div>
+                <ul class="menu-social">
+                    <li>
+                        <a href="">
+                            <i class="fas fa-heart"></i>
+                            <span>Like</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-share-alt"></i>
+                            <span>Share</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-download"></i>
+                            <span>Download</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="footer">
+    <div class="nav-logo">
+        <img src="./img/logo.png" alt="">
+    </div>
+    <p class="about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cupiditate ducimus itaque iusto molestiae nam reprehenderit sit unde. Dolores earum iure laudantium, mollitia nisi omnis quam ratione recusandae sed tempore.</p>
+    <p class="copyright">© Copyright ©2019 All rights reserved</p>
+</section>
+</body>
+</html>
